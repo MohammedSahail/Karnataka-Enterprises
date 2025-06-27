e1963f67402aa07c00f182cb87c951fb22d237aa
Mohammed Sahail <sahailmohammed786@gmail.com>
2025-06-28 00:43:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+// Dragging Support
+let navbar = document.querySelector('.draggable');
+let isDragging = false;
+let offset = { x: 0, y: 0 };
+
+navbar.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offset.x = e.clientX - navbar.getBoundingClientRect().left;
+    offset.y = e.clientY - navbar.getBoundingClientRect().top;
+    navbar.style.position = 'absolute';
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        navbar.style.left = `${e.clientX - offset.x}px`;
+        navbar.style.top = `${e.clientY - offset.y}px`;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+// Mobile Animation
+let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollTop > lastScrollTop) {
+        navbar.classList.remove('show'); // Hide on scroll down
+    } else {
+        navbar.classList.add('show'); // Show on scroll up
+    }
+    lastScrollTop = currentScrollTop;
+});
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
